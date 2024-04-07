901eab4f208cebe2ad076a8c128f40afddae5733
muhamed-droid <mmasnopita1@etf.unsa.ba>
2024-04-07 23:36:51 +0200

    Some minor frontend made and working fine

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 
 import './App.css';
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [listOfProducts, setListOfProducts] = useState([]);
+
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/products").then((response)=> {
+      setListOfProducts(response.data);
+    });
+  }, [])
   return (
     <div className="App">
-     
+     {listOfProducts.map( (value,key) => {return <div className="products"> 
+        <div className="name"> {value.name} </div> 
+        <div className="body"> {value.description} </div> 
+        <div className="price"> {value.price} </div>
+        <div className="photo"> {value.photo} </div>
+      </div>})}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors=require('cors');
 
 const db = require('./models');
 
+app.use(cors());
 app.use(express.json());
 
 //Routers 
