9d88049059b5b6ce0a04fd365e753751d72e6bb2
muhamed-droid <mmasnopita1@etf.unsa.ba>
2024-04-12 11:51:28 +0200

    kka

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
 import Home from './pages/Home';
 
 function App() {
