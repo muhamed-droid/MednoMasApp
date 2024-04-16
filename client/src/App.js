c021a17851b0fa15439aef2150312df643152271
muhamed-droid <mmasnopita1@etf.unsa.ba>
2024-04-16 09:22:39 +0200

    Everzthing is fine, but response from backend is not shown on front

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,17 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
 import Home from './pages/Home';
+import CreateProduct from './pages/CreateProduct';
 
 function App() {
 
   return (
     <div className="App">
       <Router> 
+        <Link to="/createproduct"> Create a Product</Link>
           <Routes>
             <Route path="/" exact component={Home}/>
+            <Route path="/createproduct" exact component={CreateProduct}/>
           </Routes>
       </Router>
     </div>
---
+++ client/src/pages/CreateProduct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CreateProduct() {
+  return (
+    <div>
+      CreateProduct
+    </div>
+  )
+}
+
+export default CreateProduct;
