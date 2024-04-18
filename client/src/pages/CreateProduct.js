62b63de99ee68a44416914b5e6a4c057c23bc60c
muhamed-droid <mmasnopita1@etf.unsa.ba>
2024-04-18 11:14:28 +0200

    Upload image is there, BLOB element is recieved in database. Not working while trying to display the photos.

--- client/src/pages/CreateProduct.js
+++ client/src/pages/CreateProduct.js
@@ -25,20 +25,50 @@ function CreateProduct() {
 
   return (
     <div className="createProductPage">
-        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-            <Form className="formContainer"> 
-                <label> Name: </label>
-                <ErrorMessage name="name" component="span"/>
-                <Field autocomplete="off" id= "inputCreateProduct" name="name" placeholder="(Ex. Honey...)"/>    
-                <label> Description: </label>
-                <ErrorMessage name="description" component="span"/>
-                <Field autocomplete="off" id= "inputCreateProduct" name="description" placeholder="(Ex. Honey from the deepest of Bosnian...)"/>    
-                <label> Price: </label>
-                <ErrorMessage name="price" component="span"/>
-                <Field autocomplete="off" id= "inputCreateProduct" name="price" placeholder="(Ex. 20.0...)"/>  
-                <button type="submit">Create Product </button>  
-            </Form> 
-        </Formik>
+       <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+  {({ handleSubmit, setFieldValue }) => (
+    <Form className="formContainer" onSubmit={handleSubmit}> 
+      <div>
+        <label htmlFor="name">Name:</label>
+        <ErrorMessage name="name" component="span"/>
+        <Field autocomplete="off" id="inputCreateProduct" name="name" placeholder="(Ex. Honey...)"/>    
+      </div>
+      
+      <div>
+        <label htmlFor="description">Description:</label>
+        <ErrorMessage name="description" component="span"/>
+        <Field autocomplete="off" id="inputCreateProduct" name="description" placeholder="(Ex. Honey from the deepest of Bosnian...)"/>    
+      </div>
+
+      <div>
+        <label htmlFor="price">Price:</label>
+        <ErrorMessage name="price" component="span"/>
+        <Field autocomplete="off" id="inputCreateProduct" name="price" placeholder="(Ex. 20.0...)"/>  
+      </div>
+
+      <div>
+        <label htmlFor="photo">Add a photo here:</label>
+        <ErrorMessage name="photo" component="span"/>
+        <input
+          type="file"
+          accept="image/*"
+          id="photoInput"
+          name="photo"
+          style={{ display: 'none' }}
+          onChange={(event) => {
+            setFieldValue("photo", event.currentTarget.files[0]);
+            document.getElementById("selectedFileName").innerText = event.currentTarget.files[0].name;
+          }}
+        />
+        <label htmlFor="photoInput" id="photoInputLabel" className="photoInputLabel">Choose a file</label>
+        <span id="selectedFileName" className="selectedFileName"></span>
+      </div>
+
+      <button type="submit">Create Product</button>  
+    </Form>
+  )}
+</Formik>
+
     </div>
   )
 }
